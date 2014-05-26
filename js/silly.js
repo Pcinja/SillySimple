865d9c093aabd03cdc0b2eb3a0226cec106e1558
unknown <gorantra@LDBG274971.it.telekom.yu>
2014-05-27 00:59:39 +0200

    It's silly really

---
+++ js/silly.js
@@ -0,0 +1,30 @@
+﻿/* 
+	Vidi http://psjplearn.blogspot.com/2013/06/javascript-namespacemodule-example.html 
+*/
+
+
+var SILLY = (function (module) {
+  
+  module.DoIt = function (resultObject) {
+	  resultObject.prepend(Date() + '<br>');
+  };
+  
+  return module;
+} (SILLY || (new Object())));  // "{}" means "new Object()". It is a shortcut
+
+//NOTE: The last line could be :  (SILLY || {}) => (SILLY || (new Object())));
+
+
+
+/*	
+var SILLY = (function() {
+  
+  if (SILLY === undefined) {
+	var SILLY = new Object();
+	SILLY.DoIt = function (resultObject) {
+		resultObject.prepend(Date() + '<br>');
+	};
+  }
+  return SILLY;    
+}());
+*/
